f44553a36b3d6f0519d4f9432dc991f5bc2e7544
DragonLee <806966854@qq.com>
2021-11-22 17:01:49 +0800

    Update plusOne.go

--- week1/plusOne.go
+++ week1/plusOne.go
@@ -1,1 +1,24 @@
 
+func plusOne(digits []int) []int {
+	size := len(digits)
+
+	digits[size-1] += 1
+
+	if digits[size-1] == 10 {
+		digits[size-1] = 0
+		for i := size - 2; i >= 0; i-- {
+			if digits[i] < 9 {
+				digits[i] += 1
+				break
+			}
+			
+            digits[i] = 0
+		}
+	}
+
+	if digits[0] == 0 {
+		digits = append([]int{1}, digits[0:]...)
+	}
+
+	return digits
+}
